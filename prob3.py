52a2fd4fdd19d5e7ead41de0b2e17a53ae489196
Leonard Chan <lc599@tux64-14.cs.drexel.edu>
2016-08-18 13:05:34 -0400

    gotta increase that github score

--- prob3.py
+++ prob3.py
@@ -4,6 +4,9 @@
 from __future__ import print_function
 
 
+next_terminal = "()()"
+
+
 class SlotDefinedClass(object):
     __slots__ = tuple()
 
@@ -12,23 +15,54 @@ class SlotDefinedClass(object):
             setattr(self, attr, kwargs[attr])
 
 
-class Node(object):
+class Node(SlotDefinedClass):
     __slots__ = ("label", "leftmost_child", "right_sibling")
 
 
 def make_node0(x):
-    pass
+    root = Node(label=x, leftmost_child=None, right_sibling=None)
+    return root
 
 
 def make_node1(x, t):
-    pass
+    root = make_node0(x)
+    root.leftmost_child = t
+    return root
 
 
 def make_node4(x, t1, t2, t3, t4):
-    pass
+    root = make_node1(x, t1)
+    t1.right_sibling = t2
+    t2.right_sibling = t3
+    t3.right_sibling = t4
+    return root
+
+
+def B():
+    global next_terminal
+
+    if next_terminal[0] == "(":
+        next_terminal = next_terminal[1:]
+        first_b = B()
+        if first_b and next_terminal[0] == ")":
+            next_terminal = next_terminal[1:]
+            second_b = B()
+            if not second_b:
+                return None
+            else:
+                return make_node4("B", make_node0("("), first_b, make_node0(")"), second_b)
+        else:
+            return None
+    else:
+        return make_node1("B", make_node0("e"))
 
 
 def main():
+    global next_terminal
+    parse_tree = B()
+
+    print(parse_tree)
+
     return 0
 
 
